b3958166b094017342a452c882775476a0371da8
John Paxton <johnpaxton@gmail.com>
2023-01-27 08:58:14 -0500

    Rearrange for better deployments

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const router = jsonServer.router('data/db.json');
+const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults();
 app.use(middlewares);
 app.use(router);
